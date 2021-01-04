adeb6a9c47e661438028cbb0c6eee8f3e5fd2cd3
Joshua Lin <joshua8712101@gmail.com>
2021-01-05 06:00:53 +0800

    Edit config and metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `josh's landing page`,
+    description: `A quick landing page built with the default Gatsbyjs starter. <3 Nowara`,
+    author: `Joshua Lin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `joshua lin`,
+        short_name: `jlin`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#25625c`,
+        theme_color: `#25625c`,
         display: `minimal-ui`,
         icon: `src/assets/heyguys.png`, // This path is relative to the root of the site.
       },
